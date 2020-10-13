d756a03d2de2f89b5a1b5e760ff83c85f80366e7
Mikhail <mikhail.gureev00@gmail.com>
2020-10-13 10:19:15 +0300

    initial

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+const app = express()
+const path = require('path')
+
+app.use(express.json({extended:true}))
+app.use('/api/auth', require('./routes/auth.routs'))
+app.use('/api/link', require('./routes/link.routes'))
+app.use('/t/', require('./routes/redirect.routes'))
+
+if(proccess.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = config.get("PORT") || 5000
+
+async function start() {
+    try{
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+        })
+        app.listen(PORT, ()=>{
+            console.log(`app has been started ${PORT}`)
+        })
+    } catch(e) {
+        console.log('server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
